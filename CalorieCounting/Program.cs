6b63ce0e49e89cd57af3c1e1418cded8e398f90e
George Mason <george.mason@red-gate.com>
2022-12-01 14:55:47 +0000

    day 1

---
+++ CalorieCounting/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using var streamReader = new StreamReader("input.txt");
+
+var calorieCounter = new List<int>();
+
+while (!streamReader.EndOfStream)
+{
+    var totalCalories = 0;
+    while (true)
+    {
+        var calories = streamReader.ReadLine();
+        if (string.IsNullOrWhiteSpace(calories))
+            break;
+
+        totalCalories += int.Parse(calories);
+    }
+
+    calorieCounter.Add(totalCalories); 
+}
+
+var max = calorieCounter.Max();
+var elf = calorieCounter.IndexOf(max);
+Console.WriteLine($"Elf {elf} is carrying {max} calories! Talk to him for snacks!");
