3a6a59c60ab646c4ce0c36ba55dcc89730991700
George Mason <george.mason@red-gate.com>
2022-12-07 19:59:26 +0000

    day 4 part 1

---
+++ 2022/CampCleanup/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using var sr = new StreamReader("input.txt");
+
+var count = 0;
+while (!sr.EndOfStream)
+{
+    var line = sr.ReadLine();
+    var pairs = line!.Split(',');
+
+    IReadOnlyList<int> ElfRange(int elfIndex)
+    {
+        var indexes = pairs![elfIndex].Split('-');
+        var start = int.Parse(indexes[0]);
+        var end = int.Parse(indexes[1]);
+        
+        return Enumerable.Range(start, end - start + 1).ToArray();
+    }
+
+    bool Engulfs(IReadOnlyList<int> datum, IReadOnlyList<int> spec)
+    {
+        return (datum[0] <= spec[0] && datum[^1] >= spec[^1])
+               || (spec[0] <= datum[0] && spec[^1] >= datum[^1]);
+    }
+
+    var x = ElfRange(0);
+    var y = ElfRange(1);
+    if (Engulfs(x, y))
+        count++;
+}
+
+Console.WriteLine(count);
