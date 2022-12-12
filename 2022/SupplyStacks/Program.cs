7970d8b577c9eea09bf1f233f3bee5d4d18b4582
George Mason <george.mason@red-gate.com>
2022-12-12 09:40:35 +0000

    WIP

---
+++ 2022/SupplyStacks/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+using System.Text.RegularExpressions;
+
+var stacks = new List<Stack<char>>();
+using var sr = new StreamReader("input.txt");
+
+while (!sr.EndOfStream)
+{
+    var line = sr.ReadLine();
+
+    if (!string.IsNullOrEmpty(line) && !line.StartsWith(" "))
+    {
+        var crates = line.Chunk(4).Select(crate => new String(crate));
+
+        var i = 0;
+        foreach (var crate in crates)
+        {
+            if (stacks.Count < i + 1)
+                stacks.Add(new Stack<char>());
+
+            if (char.TryParse(Regex.Match(crate!, "\\w").Value, out var item))
+                stacks[i].Push(item);
+
+            i++;
+        }
+    }
+    else
+    {
+        var x = 1;
+        // process to movement with crane        
+    }
+    
+}
